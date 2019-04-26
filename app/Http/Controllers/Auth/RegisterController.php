a05e86b922fc818269cf4cdcb701b56c606e99c7
aleph1507 <xrristo@gmail.com>
2019-04-26 19:22:38 +0200

    u0001

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -49,8 +49,9 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
+//            'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'language' => 'required|in:french,dutch',
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
     }
@@ -64,9 +65,10 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return User::create([
-            'name' => $data['name'],
+//            'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'language' => $data['language']
         ]);
     }
 }
